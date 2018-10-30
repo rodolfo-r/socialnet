997b6fbcde3087a60179f2e0340e15fbd7e2de00
Rodolfo Rodriguez <rodolfo.rodrigarci@gmail.com>
2018-10-30 11:58:09 -0500

    Logout user

--- static/_shared/header.js
+++ static/_shared/header.js
@@ -2,6 +2,8 @@ const username = parseJWT(getCookie('socialnet_token')).usn
 document.querySelector('#header-profile img').setAttribute('src', `http://localhost:3001/files/user/${username}/profile.jpg`)
 document.querySelector('#header-profile a').setAttribute('href', `http://localhost:3000/user/${username}`)
 
+document.querySelector('#logout').addEventListener('click', logout)
+
 function getCookie(name) {
   const value = '; ' + document.cookie;
   const parts = value.split('; ' + name + '=')
@@ -12,4 +14,9 @@ function parseJWT(token) {
   const base64Url = token.split('.')[1]
   const base64 = base64Url.replace('-', '+').replace('_', '/')
   return JSON.parse(window.atob(base64))
+}
+
+function logout() {
+ document.cookie = ''
+  window.location = '/'
 }
